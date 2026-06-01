c9c2d4d9041545e056627ba3553cc1f2a15fa113
Tiago Correia <tiagocorreiabispo14@gmail.com>
2026-06-01 12:15:30 -0300

    adicionado pequeno comentario ao codigo de funcionario

--- src/Entidades/Funcionario.java
+++ src/Entidades/Funcionario.java
@@ -1,6 +1,6 @@
 package Entidades;
 
 public class Funcionario {
-    //Aplicar polimorfismo em funcionario dividindo mais classes com tipos de funcionairos (areas)
+    //Aplicar polimorfismo em funcionario dividindo mais classes com tipos de funcionairos (areas) ex: Funcionario f = new Secretaria();
     //A classe extende pessoa e implementa Pagavevel
 }
