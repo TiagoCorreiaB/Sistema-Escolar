b1065b8b3d5582d037c895004e26cdf0269bec74
Tiago Correia <tiagocorreiabispo14@gmail.com>
2026-06-01 22:39:59 -0300

    outro comentario

--- src/Entidades/Funcionario.java
+++ src/Entidades/Funcionario.java
@@ -2,5 +2,22 @@ package Entidades;
 
 public class Funcionario {
     //Aplicar polimorfismo em funcionario dividindo mais classes com tipos de funcionairos (areas) ex: Funcionario f = new Secretaria();
+    //Utilizar enum assim:
+    //    public enum StatusFuncionario {
+    //    ATIVO,
+    //    FERIAS,
+    //    AFASTADO,
+    //    DESLIGADO
+
+    //}Ou assim:
+    //    public enum Departamento {
+    //    RH,
+    //    FINANCEIRO,
+    //    TI,
+    //    MARKETING
+    //}
+
+
     //A classe extende pessoa e implementa Pagavevel
+
 }
